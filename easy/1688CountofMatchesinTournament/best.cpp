f7356491870553f92a5dc83e63dd19fc204d4e25
Vilen <vilenmoscow@gmail.com>
2023-12-05 09:21:38 +0300

    Create best.cpp

---
+++ easy/1688CountofMatchesinTournament/best.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+       int cnt = 0;
+        int rev = 0;
+        while(n>1) {
+            rev = n/2;
+            cnt+=rev;
+            n=n-rev;
+        }
+        return cnt;
+    }
+};
